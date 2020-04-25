b0bcb3528b0d0704541fd1a41d9a29afbeb9191a
Michael Egorov <michwill@gmail.com>
2020-04-25 16:07:57 -0700

    All tests pass, no mysteries anymore

--- tests/test_mintable_erc20.py
+++ tests/test_mintable_erc20.py
@@ -43,5 +43,46 @@ def test_mintable_in_timeframe(w3, token):
             assert token.caller.mintable_in_timeframe(t0, t0 + dt) < rate * (dt + 1)
 
 
-def test_mints(w3, token):
-    pass
+def test_mint(w3, token):
+    owner = w3.eth.accounts[0]
+    from_owner = {'from': owner}
+    bob = w3.eth.accounts[1]
+    t0 = block_timestamp(w3)
+
+    # Sometimes can go across epochs
+    for i in range(20):
+        to_mint = token.caller.available_supply() - token.caller.totalSupply()
+        assert to_mint >= 0
+        t0 = block_timestamp(w3)
+        if to_mint > 0:
+            token.functions.mint(owner, to_mint).transact(from_owner)
+        # All minted before t0 (including t0)
+        # Blocks move by 1 s here, so cannot mint rate + 1
+        to_mint = token.caller.available_supply() - token.caller.totalSupply()
+        rate = token.caller.rate()
+        with pytest.raises(TransactionFailed):
+            # Sometimes rate decreases in this block - that tx will fail too
+            token.functions.mint(owner, to_mint + rate + 1).transact(from_owner)
+        t0 = block_timestamp(w3) + 1
+
+        t1 = time_travel(w3, int(10 ** (random() * log10(300 * 86400)))) + 1  # next block
+        balance_before = token.caller.balanceOf(bob)
+
+        t_start = randrange(t0, t1 + 1)
+
+        if t1 > t_start:
+            dt = int(10 ** (random() * log10(t1 - t_start)))
+        else:
+            dt = 0
+        with pytest.raises(TransactionFailed):
+            # -2 (not -1) because mint tx failed and another block has passed
+            non_mintable_value = token.caller.mintable_in_timeframe(t0 - 2, t1)
+            token.functions.mint(bob, non_mintable_value).transact(from_owner)
+        value = token.caller.mintable_in_timeframe(t_start, t_start + dt)
+        value = min(value, int(value * random() * 2))
+        token.functions.mint(bob, value).transact(from_owner)
+
+        balance_after = token.caller.balanceOf(bob)
+        assert balance_after - balance_before == value
+
+        t0 = t1
