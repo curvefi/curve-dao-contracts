ee8c326b226a8a58d3a310f2305a37420aab68f2
Michael Egorov <michwill@gmail.com>
2020-04-29 23:22:46 -0700

    Initial setup

--- tests/test_gauge_controller.py
+++ tests/test_gauge_controller.py
@@ -6,7 +6,7 @@ def test_gauge_controller(w3, gauge_controller, liquidity_gauge, three_gauges):
 
     # Set up gauges and types
     gauge_controller.functions.add_type().transact(from_admin)  # 0
-    gauge_controller.functions.add_type(10 ** 17).transact(from_admin)  # 1
+    gauge_controller.functions.add_type().transact(from_admin)  # 1
     gauge_controller.functions.change_type_weight(0, type_weights[0]).transact(from_admin)
 
     gauge_controller.functions.add_gauge(three_gauges[0].address, 0).transact(from_admin)
