f0f740d0f5c448449cecba3f1c23b7c326a05c8a
Michael Egorov <michwill@gmail.com>
2020-04-29 23:34:02 -0700

    Test static parameters

--- tests/test_gauge_controller.py
+++ tests/test_gauge_controller.py
@@ -1,4 +1,7 @@
-def test_gauge_controller(w3, gauge_controller, liquidity_gauge, three_gauges):
+from .conftest import block_timestamp
+
+
+def test_gauge_controller(w3, gauge_controller, liquidity_gauge, three_gauges, token):
     admin = w3.eth.accounts[0]
     from_admin = {'from': admin}
     type_weights = [5 * 10 ** 17, 2 * 10 ** 18]
@@ -16,3 +19,20 @@ def test_gauge_controller(w3, gauge_controller, liquidity_gauge, three_gauges):
     gauge_controller.functions.change_type_weight(1, type_weights[1]).transact(from_admin)
 
     gauge_controller.functions.change_gauge_weight(three_gauges[0].address, gauge_weights[0]).transact(from_admin)
+
+    last_change = block_timestamp(w3)
+    assert last_change == gauge_controller.caller.last_change()
+    assert gauge_controller.caller.start_epoch_time() == token.caller.start_epoch_time()
+
+    # Check matcing of all patameters and incremental calculations
+    assert gauge_controller.caller.n_gauge_types() == 2
+    assert gauge_controller.caller.n_gauges() == 3
+    for i in range(3):
+        assert three_gauges[i].address == gauge_controller.caller.gauges(i)
+    assert gauge_controller.caller.gauge_types(three_gauges[0].address) == 0
+    assert gauge_controller.caller.gauge_types(three_gauges[1].address) == 0
+    assert gauge_controller.caller.gauge_types(three_gauges[2].address) == 1
+    for i in range(3):
+        assert gauge_controller.caller.gauge_weights(three_gauges[i].address) == gauge_weights[i]
+    for i in range(2):
+        assert gauge_controller.caller.type_weights(i) == type_weights[i]
