c59aac50d8020a543572b3d6b518933eb8548064
Ben Hauser <ben@hauser.id>
2020-06-08 23:26:48 +0400

    test: LiquidityGauge tests

--- tests/unitary/LiquidityGauge/test_checkpoint.py
+++ tests/unitary/LiquidityGauge/test_checkpoint.py
@@ -1,5 +1,17 @@
 import brownie
 
+YEAR = 86400 * 365
+
+
+def test_user_checkpoint(accounts, liquidity_gauge):
+    liquidity_gauge.user_checkpoint(accounts[1], {'from': accounts[1]})
+
+
+def test_user_checkpoint_new_period(accounts, rpc, liquidity_gauge):
+    liquidity_gauge.user_checkpoint(accounts[1], {'from': accounts[1]})
+    rpc.sleep(int(YEAR * 1.1))
+    liquidity_gauge.user_checkpoint(accounts[1], {'from': accounts[1]})
+
 
 def test_user_checkpoint_wrong_account(accounts, liquidity_gauge):
     with brownie.reverts("dev: unauthorized"):
