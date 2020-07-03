300012748c19ff35c9f7660e98e852d1dd1ab6d5
AA <angelangelov97@gmail.com>
2020-07-03 19:37:40 +0300

    Add required_confs and deploy PoolProxy

--- scripts/deploy_testnet.py
+++ scripts/deploy_testnet.py
@@ -6,7 +6,7 @@ from web3.gas_strategies.time_based import fast_gas_price_strategy as gas_strate
 from brownie import (
         web3, accounts,
         ERC20CRV, VotingEscrow, ERC20, ERC20LP, CurvePool, Registry,
-        GaugeController, Minter, LiquidityGauge
+        GaugeController, Minter, LiquidityGauge, PoolProxy
         )
 
 
@@ -22,6 +22,8 @@ DISTRIBUTION_AMOUNT = 10 ** 6 * 10 ** 18
 DISTRIBUTION_ADDRESSES = ["0x39415255619783A2E71fcF7d8f708A951d92e1b6", "0x6cd85bbb9147b86201d882ae1068c67286855211"]
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+CONFS = 1
 
 
 def repeat(f, *args):
@@ -41,29 +43,29 @@ def save_abi(contract, name):
 
 
 def deploy_erc20s_and_pool(deployer):
-    coin_a = repeat(ERC20.deploy, "Coin A", "USDA", 18, {'from': deployer})
-    repeat(coin_a._mint_for_testing, 10 ** 9 * 10 ** 18, {'from': deployer})
-    coin_b = repeat(ERC20.deploy, "Coin B", "USDB", 18, {'from': deployer})
-    repeat(coin_b._mint_for_testing, 10 ** 9 * 10 ** 18, {'from': deployer})
+    coin_a = repeat(ERC20.deploy, "Coin A", "USDA", 18, {'from': deployer, 'required_confs': CONFS})
+    repeat(coin_a._mint_for_testing, 10 ** 9 * 10 ** 18, {'from': deployer, 'required_confs': CONFS})
+    coin_b = repeat(ERC20.deploy, "Coin B", "USDB", 18, {'from': deployer, 'required_confs': CONFS})
+    repeat(coin_b._mint_for_testing, 10 ** 9 * 10 ** 18, {'from': deployer, 'required_confs': CONFS})
 
-    lp_token = repeat(ERC20LP.deploy, "Some pool", "cPool", 18, 0, {'from': deployer})
+    lp_token = repeat(ERC20LP.deploy, "Some pool", "cPool", 18, 0, {'from': deployer, 'required_confs': CONFS})
     save_abi(lp_token, 'lp_token')
-    pool = repeat(CurvePool.deploy, [coin_a, coin_b], lp_token, 100, 4 * 10 ** 6, {'from': deployer})
+    pool = repeat(CurvePool.deploy, [coin_a, coin_b], lp_token, 100, 4 * 10 ** 6, {'from': deployer, 'required_confs': CONFS})
     save_abi(pool, 'curve_pool')
-    repeat(lp_token.set_minter, pool, {'from': deployer})
+    repeat(lp_token.set_minter, pool, {'from': deployer, 'required_confs': CONFS})
 
-    registry = repeat(Registry.deploy, [ZERO_ADDRESS] * 4, {'from': deployer})
+    registry = repeat(Registry.deploy, [ZERO_ADDRESS] * 4, {'from': deployer, 'required_confs': CONFS})
     save_abi(registry, 'registry')
 
     for account in DISTRIBUTION_ADDRESSES:
-        repeat(coin_a.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer})
-        repeat(coin_b.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer})
+        repeat(coin_a.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer, 'required_confs': CONFS})
+        repeat(coin_b.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer, 'required_confs': CONFS})
 
-    repeat(pool.commit_transfer_ownership, ARAGON_AGENT, {'from': deployer})
-    repeat(pool.apply_transfer_ownership, {'from': deployer})
+    repeat(pool.commit_transfer_ownership, ARAGON_AGENT, {'from': deployer, 'required_confs': CONFS})
+    repeat(pool.apply_transfer_ownership, {'from': deployer, 'required_confs': CONFS})
 
-    repeat(registry.commit_transfer_ownership, ARAGON_AGENT, {'from': deployer})
-    repeat(registry.apply_transfer_ownership, {'from': deployer})
+    repeat(registry.commit_transfer_ownership, ARAGON_AGENT, {'from': deployer, 'required_confs': CONFS})
+    repeat(registry.apply_transfer_ownership, {'from': deployer, 'required_confs': CONFS})
 
     return lp_token
 
@@ -81,30 +83,32 @@ def main():
 
     lp_token = deploy_erc20s_and_pool(deployer)
 
-    token = repeat(ERC20CRV.deploy, "Curve DAO Token", "CRV", 18, 10 ** 9, {'from': deployer})
+    token = repeat(ERC20CRV.deploy, "Curve DAO Token", "CRV", 18, 10 ** 9, {'from': deployer, 'required_confs': CONFS})
     save_abi(token, 'token_crv')
 
-    escrow = repeat(VotingEscrow.deploy, token, "Vote-escrowed CRV", "veCRV", "veCRV_0.99", {'from': deployer})
+    escrow = repeat(VotingEscrow.deploy, token, "Vote-escrowed CRV", "veCRV", "veCRV_0.99", {'from': deployer, 'required_confs': CONFS})
     save_abi(escrow, 'voting_escrow')
 
-    repeat(escrow.changeController, TOKEN_MANAGER, {'from': deployer})
+    repeat(escrow.changeController, TOKEN_MANAGER, {'from': deployer, 'required_confs': CONFS})
 
     for account in DISTRIBUTION_ADDRESSES:
-        repeat(token.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer})
+        repeat(token.transfer, account, DISTRIBUTION_AMOUNT, {'from': deployer, 'required_confs': CONFS})
 
-    gauge_controller = repeat(GaugeController.deploy, token, escrow, {'from': deployer})
+    gauge_controller = repeat(GaugeController.deploy, token, escrow, {'from': deployer, 'required_confs': CONFS})
     save_abi(gauge_controller, 'gauge_controller')
 
-    minter = repeat(Minter.deploy, token, gauge_controller, {'from': deployer})
+    minter = repeat(Minter.deploy, token, gauge_controller, {'from': deployer, 'required_confs': CONFS})
     save_abi(minter, 'minter')
 
-    liquidity_gauge = repeat(LiquidityGauge.deploy, lp_token, minter, {'from': deployer})
+    liquidity_gauge = repeat(LiquidityGauge.deploy, lp_token, minter, {'from': deployer, 'required_confs': CONFS})
     save_abi(liquidity_gauge, 'liquidity_gauge')
 
-    repeat(token.set_minter, minter, {'from': deployer})
-    repeat(gauge_controller.add_type, b'Liquidity', {'from': deployer})
-    repeat(gauge_controller.change_type_weight, 0, 10 ** 18, {'from': deployer})
-    repeat(gauge_controller.add_gauge, liquidity_gauge, 0, 10 ** 18, {'from': deployer})
+    repeat(token.set_minter, minter, {'from': deployer, 'required_confs': CONFS})
+    repeat(gauge_controller.add_type, b'Liquidity', {'from': deployer, 'required_confs': CONFS})
+    repeat(gauge_controller.change_type_weight, 0, 10 ** 18, {'from': deployer, 'required_confs': CONFS})
+    repeat(gauge_controller.add_gauge, liquidity_gauge, 0, 10 ** 18, {'from': deployer, 'required_confs': CONFS})
 
-    repeat(gauge_controller.transfer_ownership, ARAGON_AGENT, {'from': deployer})
-    repeat(escrow.transfer_ownership, ARAGON_AGENT, {'from': deployer})
+    repeat(gauge_controller.transfer_ownership, ARAGON_AGENT, {'from': deployer, 'required_confs': CONFS})
+    repeat(escrow.transfer_ownership, ARAGON_AGENT, {'from': deployer, 'required_confs': CONFS})
+
+    repeat(PoolProxy.deploy, {'from': deployer, 'required_confs': CONFS})
