4cd4dab365ca32d1f9d730cef9de4ca953b6cfd0
Michael Egorov <michwill@gmail.com>
2020-10-25 17:45:02 +0100

    Parallel fee distribution for 2 accounts tested

--- tests/unitary/FeeDistribution/test_fee_distribution.py
+++ tests/unitary/FeeDistribution/test_fee_distribution.py
@@ -109,4 +109,33 @@ def test_deposited_twice(web3, chain, accounts, voting_escrow, fee_distributor, 
     tokens_to_exclude = fee_distributor.tokens_per_week(exclude_time)
     assert abs(10**19 - coin_a.balanceOf(alice) - tokens_to_exclude) < 10
 
-# TODO: parallel deposits by 2 LPs
+
+def test_deposited_parallel(web3, chain, accounts, voting_escrow, fee_distributor, coin_a, token):
+    alice, bob, charlie = accounts[0:3]
+    amount = 1000 * 10 ** 18
+
+    token.approve(voting_escrow.address, amount * 10, {'from': alice})
+    token.approve(voting_escrow.address, amount * 10, {'from': bob})
+    token.transfer(bob, amount, {'from': alice})
+    coin_a._mint_for_testing(100 * 10 ** 18, {'from': charlie})
+
+    voting_escrow.create_lock(amount, chain[-1].timestamp + 8*WEEK, {'from': alice})
+    voting_escrow.create_lock(amount, chain[-1].timestamp + 8*WEEK, {'from': bob})
+    chain.sleep(WEEK)
+    chain.mine()
+    start_time = int(chain.time())
+    chain.sleep(WEEK*5)
+
+    fee_distributor = fee_distributor(t=start_time)
+    coin_a.transfer(fee_distributor, 10**19, {'from': charlie})
+    fee_distributor.checkpoint_token()
+    chain.sleep(WEEK)
+    fee_distributor.checkpoint_token()
+
+    fee_distributor.claim({'from': alice})
+    fee_distributor.claim({'from': bob})
+
+    balance_alice = coin_a.balanceOf(alice)
+    balance_bob = coin_a.balanceOf(bob)
+    assert balance_alice == balance_bob
+    assert abs(balance_alice + balance_bob - 10**19) < 10
