c01724d4054819a0a5b446e44549455acb4d07cc
Ben Hauser <ben@hauser.id>
2020-11-01 22:09:42 +0400

    test: reward gauge wrapper tests

---
+++ tests/unitary/LiquidityGaugeRewardWrapper/test_approve.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import brownie
+import pytest
+
+
+@pytest.mark.parametrize("idx", range(5))
+def test_initial_approval_is_zero(reward_gauge_wrapper, accounts, idx):
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[idx]) == 0
+
+
+def test_approve(reward_gauge_wrapper, accounts):
+    reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[1]) == 10**19
+
+
+def test_modify_approve(reward_gauge_wrapper, accounts):
+    reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+    reward_gauge_wrapper.approve(accounts[1], 12345678, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[1]) == 12345678
+
+
+def test_revoke_approve(reward_gauge_wrapper, accounts):
+    reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+    reward_gauge_wrapper.approve(accounts[1], 0, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[1]) == 0
+
+
+def test_approve_self(reward_gauge_wrapper, accounts):
+    reward_gauge_wrapper.approve(accounts[0], 10**19, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[0]) == 10**19
+
+
+def test_only_affects_target(reward_gauge_wrapper, accounts):
+    reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[1], accounts[0]) == 0
+
+
+def test_returns_true(reward_gauge_wrapper, accounts):
+    tx = reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+
+    assert tx.return_value is True
+
+
+def test_approval_event_fires(accounts, reward_gauge_wrapper):
+    tx = reward_gauge_wrapper.approve(accounts[1], 10**19, {'from': accounts[0]})
+
+    assert len(tx.events) == 1
+    assert tx.events["Approval"].values() == [accounts[0], accounts[1], 10**19]
+
+
+def test_increase_allowance(accounts, reward_gauge_wrapper):
+    reward_gauge_wrapper.approve(accounts[1], 100, {'from': accounts[0]})
+    reward_gauge_wrapper.increaseAllowance(accounts[1], 403, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[1]) == 503
+
+
+def test_decrease_allowance(accounts, reward_gauge_wrapper):
+    reward_gauge_wrapper.approve(accounts[1], 100, {'from': accounts[0]})
+    reward_gauge_wrapper.decreaseAllowance(accounts[1], 34, {'from': accounts[0]})
+
+    assert reward_gauge_wrapper.allowance(accounts[0], accounts[1]) == 66
+
---
+++ tests/unitary/LiquidityGaugeRewardWrapper/test_deposit_withdraw.py
@@ -0,0 +1,116 @@
+import brownie
+import pytest
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(accounts, gauge_controller, minter, liquidity_gauge_reward, reward_gauge_wrapper, token, mock_lp_token):
+    token.set_minter(minter, {'from': accounts[0]})
+
+    gauge_controller.add_type(b'Liquidity', 10**10, {'from': accounts[0]})
+    gauge_controller.add_gauge(liquidity_gauge_reward, 0, 0, {'from': accounts[0]})
+
+    mock_lp_token.approve(reward_gauge_wrapper, 2 ** 256 - 1, {"from": accounts[0]})
+
+
+def test_deposit(accounts, liquidity_gauge_reward, reward_gauge_wrapper, reward_contract, mock_lp_token):
+    balance = mock_lp_token.balanceOf(accounts[0])
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+
+    assert mock_lp_token.balanceOf(reward_contract) == 100000
+    assert mock_lp_token.balanceOf(reward_gauge_wrapper) == 0
+    assert mock_lp_token.balanceOf(accounts[0]) == balance - 100000
+
+    assert liquidity_gauge_reward.totalSupply() == 100000
+    assert liquidity_gauge_reward.balanceOf(reward_gauge_wrapper) == 100000
+
+    assert reward_gauge_wrapper.totalSupply() == 100000
+    assert reward_gauge_wrapper.balanceOf(accounts[0]) == 100000
+
+
+def test_deposit_zero(accounts, liquidity_gauge_reward, reward_gauge_wrapper, reward_contract, mock_lp_token):
+    balance = mock_lp_token.balanceOf(accounts[0])
+    liquidity_gauge_reward.deposit(0, {'from': accounts[0]})
+
+    assert mock_lp_token.balanceOf(reward_contract) == 0
+    assert mock_lp_token.balanceOf(reward_gauge_wrapper) == 0
+    assert mock_lp_token.balanceOf(accounts[0]) == balance
+
+    assert liquidity_gauge_reward.totalSupply() == 0
+    assert liquidity_gauge_reward.balanceOf(reward_gauge_wrapper) == 0
+
+    assert reward_gauge_wrapper.totalSupply() == 0
+    assert reward_gauge_wrapper.balanceOf(accounts[0]) == 0
+
+
+def test_deposit_insufficient_balance(accounts, reward_gauge_wrapper):
+    with brownie.reverts():
+        reward_gauge_wrapper.deposit(100000, {'from': accounts[1]})
+
+
+def test_withdraw(accounts, liquidity_gauge_reward, reward_gauge_wrapper, reward_contract, mock_lp_token):
+    balance = mock_lp_token.balanceOf(accounts[0])
+
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+    reward_gauge_wrapper.withdraw(100000, {'from': accounts[0]})
+
+    assert mock_lp_token.balanceOf(reward_contract) == 0
+    assert mock_lp_token.balanceOf(reward_gauge_wrapper) == 0
+    assert mock_lp_token.balanceOf(accounts[0]) == balance
+
+    assert liquidity_gauge_reward.totalSupply() == 0
+    assert liquidity_gauge_reward.balanceOf(reward_gauge_wrapper) == 0
+
+    assert reward_gauge_wrapper.totalSupply() == 0
+    assert reward_gauge_wrapper.balanceOf(accounts[0]) == 0
+
+
+def test_withdraw_zero(accounts, liquidity_gauge_reward, reward_gauge_wrapper, reward_contract, mock_lp_token):
+    balance = mock_lp_token.balanceOf(accounts[0])
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+    reward_gauge_wrapper.withdraw(0, {'from': accounts[0]})
+
+    assert mock_lp_token.balanceOf(reward_contract) == 100000
+    assert mock_lp_token.balanceOf(reward_gauge_wrapper) == 0
+    assert mock_lp_token.balanceOf(accounts[0]) == balance - 100000
+
+    assert liquidity_gauge_reward.totalSupply() == 100000
+    assert liquidity_gauge_reward.balanceOf(reward_gauge_wrapper) == 100000
+
+    assert reward_gauge_wrapper.totalSupply() == 100000
+    assert reward_gauge_wrapper.balanceOf(accounts[0]) == 100000
+
+
+def test_withdraw_new_epoch(accounts, chain, liquidity_gauge_reward, reward_gauge_wrapper, reward_contract, mock_lp_token):
+    balance = mock_lp_token.balanceOf(accounts[0])
+
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+    chain.sleep(86400 * 400)
+    reward_gauge_wrapper.withdraw(100000, {'from': accounts[0]})
+
+    assert mock_lp_token.balanceOf(reward_contract) == 0
+    assert mock_lp_token.balanceOf(reward_gauge_wrapper) == 0
+    assert mock_lp_token.balanceOf(accounts[0]) == balance
+
+    assert liquidity_gauge_reward.totalSupply() == 0
+    assert liquidity_gauge_reward.balanceOf(reward_gauge_wrapper) == 0
+
+    assert reward_gauge_wrapper.totalSupply() == 0
+    assert reward_gauge_wrapper.balanceOf(accounts[0]) == 0
+
+
+def test_transfer_and_withdraw(accounts, reward_gauge_wrapper, mock_lp_token):
+
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+    reward_gauge_wrapper.transfer(accounts[1], 100000)
+    reward_gauge_wrapper.withdraw(100000, {'from': accounts[1]})
+
+    assert mock_lp_token.balanceOf(accounts[1]) == 100000
+
+
+def test_cannot_withdraw_after_transfer(accounts, reward_gauge_wrapper, mock_lp_token):
+
+    reward_gauge_wrapper.deposit(100000, {'from': accounts[0]})
+    reward_gauge_wrapper.transfer(accounts[1], 100000)
+
+    with brownie.reverts():
+        reward_gauge_wrapper.withdraw(100000, {'from': accounts[0]})
