d599b9b9f7117a45f3d5de834a2784a164b63816
Ben Hauser <ben@hauser.id>
2020-11-11 01:04:00 +0400

    test: burn and execute

--- tests/fork/Burners/test_btc_burner.py
+++ tests/fork/Burners/test_btc_burner.py
@@ -17,15 +17,17 @@ tokens = (
 
 
 @pytest.mark.parametrize("token", [i[0] for i in tokens], ids=[i[1] for i in tokens])
-@pytest.mark.parametrize("has_balance", (True, False))
-def test_swap(MintableTestToken, SUSD, alice, receiver, burner, token, has_balance):
+@pytest.mark.parametrize("burner_balance", (True, False))
+@pytest.mark.parametrize("caller_balance", (True, False))
+def test_swap(MintableTestToken, SUSD, alice, receiver, burner, token, burner_balance, caller_balance):
     coin = MintableTestToken(token)
     amount = 10**coin.decimals()
 
-    coin._mint_for_testing(alice, amount, {'from': alice})
-    coin.approve(burner, 2**256-1, {'from': alice})
+    if caller_balance:
+        coin._mint_for_testing(alice, amount, {'from': alice})
+        coin.approve(burner, 2**256-1, {'from': alice})
 
-    if has_balance:
+    if burner_balance:
         coin._mint_for_testing(burner, amount, {'from': alice})
 
     burner.burn(coin, {'from': alice})
@@ -43,6 +45,33 @@ def test_execute(SUSD, USDC, alice, burner, receiver):
     SUSD._mint_for_testing(burner, 10**18, {'from': alice})
 
     burner.execute({'from': alice})
+
+    assert SUSD.balanceOf(alice) == 0
+    assert SUSD.balanceOf(burner) == 0
+    assert SUSD.balanceOf(receiver) == 0
+
+    assert USDC.balanceOf(alice) == 0
+    assert USDC.balanceOf(burner) == 0
+    assert USDC.balanceOf(receiver) > 0
+
+
+@pytest.mark.parametrize("token", [i[0] for i in tokens], ids=[i[1] for i in tokens])
+def test_swap_and_execute(MintableTestToken, SUSD, USDC, alice, receiver, burner, token, chain):
+    coin = MintableTestToken(token)
+    amount = 10**coin.decimals()
+
+    coin._mint_for_testing(alice, amount, {'from': alice})
+    coin.approve(burner, 2**256-1, {'from': alice})
+
+    burner.burn(coin, {'from': alice})
+
+    chain.sleep(300)
+
+    burner.execute({'from': alice})
+
+    assert coin.balanceOf(alice) == 0
+    assert coin.balanceOf(burner) == 0
+    assert coin.balanceOf(receiver) == 0
 
     assert SUSD.balanceOf(alice) == 0
     assert SUSD.balanceOf(burner) == 0
