b7e0f5d85c990e1423fd452e20ba67b061f5d218
Edward Amor <edward.amor3@gmail.com>
2021-05-11 19:28:08 -0400

    fix: modify checkpoitn test setup

--- tests/unitary/Sidechain/test_root_checkpoint.py
+++ tests/unitary/Sidechain/test_root_checkpoint.py
@@ -1,20 +1,24 @@
 import pytest
+import math
 
 WEEK = 7 * 86400
 
 
+@pytest.fixture(autouse=True)
+def local_setup(alice, chain, gauge_controller, liquidity_gauge, root_gauge, token, minter):
+    token.set_minter(minter, {"from": alice})
+    chain.mine(timedelta=WEEK)
+    token.update_mining_parameters({"from": alice})
+
+    gauge_controller.add_type("Test", 10 ** 18, {"from": alice})
+    gauge_controller.add_gauge(liquidity_gauge, 0, 0, {"from": alice})
+    gauge_controller.add_gauge(root_gauge, 0, 1, {"from": alice})
+
+    chain.mine(timedelta=WEEK)
+
+
 @pytest.mark.skip_coverage
-def test_relative_weight_write(accounts, chain, gauge_controller, liquidity_gauge, root_gauge, token, minter):
-    token.set_minter(minter, {'from': accounts[0]})
-    chain.mine(timedelta=WEEK)
-    token.update_mining_parameters({'from': accounts[0]})
-
-    gauge_controller.add_type("Test", 10**18, {'from': accounts[0]})
-    gauge_controller.add_gauge(liquidity_gauge, 0, 0, {"from": accounts[0]})
-    gauge_controller.add_gauge(root_gauge, 0, 1, {"from": accounts[0]})
-
-    chain.mine(timedelta=WEEK)
-
+def test_relative_weight_write(alice, chain, gauge_controller, liquidity_gauge, root_gauge, token):
     rate = token.rate()
     total_emissions = 0
     assert rate > 0
@@ -24,12 +28,13 @@ def test_relative_weight_write(accounts, chain, gauge_controller, liquidity_gaug
         root_gauge.checkpoint()
         new_emissions = root_gauge.emissions() - total_emissions
         expected = rate * WEEK // i
-        assert abs(new_emissions - expected) / expected < 0.0001
+
+        assert math.isclose(new_emissions, expected)
 
         total_emissions += new_emissions
         rate = token.rate()
 
         # increaseing the gauge weight on `liquidity_gauge` each week reducees
         # the expected emission for `root_gauge` in the following week
-        gauge_controller.change_gauge_weight(liquidity_gauge, i, {'from': accounts[0]})
+        gauge_controller.change_gauge_weight(liquidity_gauge, i, {"from": alice})
         chain.mine(timedelta=WEEK)
