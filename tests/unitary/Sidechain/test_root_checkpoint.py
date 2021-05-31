80b7fac027c6a076f54780390396399fdae3008c
Ben Hauser <ben@hauser.id>
2021-06-01 01:08:53 +0400

    test: update root gauge tests

--- tests/unitary/Sidechain/test_root_checkpoint.py
+++ tests/unitary/Sidechain/test_root_checkpoint.py
@@ -1,41 +1,80 @@
 import math
 
 import pytest
+from brownie import chain
 
 WEEK = 7 * 86400
+YEAR = 365 * 86400
 
 
-@pytest.fixture(autouse=True)
-def local_setup(alice, chain, gauge_controller, liquidity_gauge, root_gauge, token, minter):
-    token.set_minter(minter, {"from": alice})
-    chain.mine(timedelta=WEEK)
-    token.update_mining_parameters({"from": alice})
-
+@pytest.mark.skip_coverage
+def test_emissions_against_expected(alice, gauge_controller, liquidity_gauge, root_gauge, token):
     gauge_controller.add_type("Test", 10 ** 18, {"from": alice})
     gauge_controller.add_gauge(liquidity_gauge, 0, 0, {"from": alice})
     gauge_controller.add_gauge(root_gauge, 0, 1, {"from": alice})
 
     chain.mine(timedelta=WEEK)
 
-
-@pytest.mark.skip_coverage
-def test_relative_weight_write(alice, chain, gauge_controller, liquidity_gauge, root_gauge, token):
     rate = token.rate()
     total_emissions = 0
     assert rate > 0
 
+    # 110 weeks ensures we see 2 reductions in the rate
     for i in range(1, 110):
-        # 110 weeks ensures we see 2 reductions in the rate
-        root_gauge.checkpoint()
-        new_emissions = root_gauge.emissions() - total_emissions
-        expected = rate * WEEK // i
 
+        this_week = chain[-1].timestamp // WEEK * WEEK
+        next_week = this_week + WEEK
+        start_epoch_time = root_gauge.start_epoch_time()
+        future_epoch_time = start_epoch_time + YEAR
+
+        # calculate the expected emissions, and checkpoint to update actual emissions
+        if next_week > future_epoch_time:
+            expected = rate * (future_epoch_time - this_week) // i
+            root_gauge.checkpoint()
+            rate = token.rate()
+            expected += rate * (next_week - future_epoch_time) // i
+        else:
+            expected = rate * WEEK // i
+            root_gauge.checkpoint()
+
+        # actual emissions should equal expected emissions
+        new_emissions = root_gauge.emissions() - total_emissions
         assert math.isclose(new_emissions, expected)
 
         total_emissions += new_emissions
-        rate = token.rate()
 
-        # increaseing the gauge weight on `liquidity_gauge` each week reducees
+        # increasing the gauge weight on `liquidity_gauge` each week reducees
         # the expected emission for `root_gauge` in the following week
         gauge_controller.change_gauge_weight(liquidity_gauge, i, {"from": alice})
         chain.mine(timedelta=WEEK)
+
+        assert rate == token.rate()
+
+
+@pytest.mark.skip_coverage
+def test_emissions_against_other_gauge(
+    alice, mock_lp_token, gauge_controller, liquidity_gauge, root_gauge
+):
+    gauge_controller.add_type("Test", 10 ** 18, {"from": alice})
+    gauge_controller.add_gauge(liquidity_gauge, 0, 1, {"from": alice})
+    gauge_controller.add_gauge(root_gauge, 0, 1, {"from": alice})
+
+    # alice is the only staker in `liquidity_gauge`
+    mock_lp_token.approve(liquidity_gauge, 2 ** 256 - 1, {"from": alice})
+    tx = liquidity_gauge.deposit(100000, {"from": alice})
+
+    # sleep until the start of the next epoch week
+    chain.mine(timestamp=(tx.timestamp // WEEK + 1) * WEEK)
+
+    # 110 weeks ensures we see 2 reductions in the rate
+    for i in range(1, 110):
+        # checkpointing the root gauge mints all the allocated emissions for the next week
+        tx = root_gauge.checkpoint()
+        emissions = root_gauge.emissions()
+
+        # now we time travel one week, and get the claimable rewards from the regular gauge
+        chain.mine(timestamp=(tx.timestamp // WEEK + 1) * WEEK)
+        claimable = liquidity_gauge.claimable_tokens(alice, {"from": alice}).return_value
+
+        # root gauge emissions should be equal to regular gauge claimable amount
+        assert math.isclose(emissions, claimable, rel_tol=1e-6)
