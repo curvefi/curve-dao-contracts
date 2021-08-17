52c633eafc17bd0777234eb1270e439bcbfde883
ksirin <ksirin@ksirin>
2021-08-16 22:39:29 -0500

    test_distribute.py done

--- tests/unitary/LixDistributor/test_distribute.py
+++ tests/unitary/LixDistributor/test_distribute.py
@@ -12,9 +12,7 @@ WEEK = 7 * 86400
 
 
 @pytest.fixture(scope="module", autouse=True)
-def minter_setup(accounts, mock_lp_token, token, minter, gauge_controller, three_gauges, chain):
-    token.set_minter(minter, {"from": accounts[0]})
-
+def distributor_setup(accounts, mock_lp_token, gauge_controller, three_gauges, chain):
     # ensure the tests all begin at the start of the epoch week
     chain.mine(timestamp=(chain.time() / WEEK + 1) * WEEK)
 
@@ -37,118 +35,119 @@ def minter_setup(accounts, mock_lp_token, token, minter, gauge_controller, three
         mock_lp_token.approve(gauge, 1e18, {"from": acct})
 
 
-def test_mint(accounts, chain, three_gauges, minter, token):
+def test_distribute(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
     chain.sleep(MONTH)
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
     expected = three_gauges[0].integrate_fraction(accounts[1])
 
     assert expected > 0
     assert token.balanceOf(accounts[1]) == expected
-    assert minter.minted(accounts[1], three_gauges[0]) == expected
+    assert distributor.distributed(accounts[1], three_gauges[0]) == expected
 
 
-def test_mint_immediate(accounts, chain, three_gauges, minter, token):
+def test_distribute_immediate(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
     t0 = chain.time()
     chain.sleep((t0 + WEEK) // WEEK * WEEK - t0 + 1)  # 1 second more than enacting the weights
 
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
     balance = token.balanceOf(accounts[1])
 
     assert balance > 0
-    assert minter.minted(accounts[1], three_gauges[0]) == balance
+    assert distributor.distributed(accounts[1], three_gauges[0]) == balance
 
 
-def test_mint_multiple_same_gauge(accounts, chain, three_gauges, minter, token):
+def test_distribute_multiple_same_gauge(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
     chain.sleep(MONTH)
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
     balance = token.balanceOf(accounts[1])
 
     chain.sleep(MONTH)
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
     expected = three_gauges[0].integrate_fraction(accounts[1])
     final_balance = token.balanceOf(accounts[1])
 
     assert final_balance > balance
     assert final_balance == expected
-    assert minter.minted(accounts[1], three_gauges[0]) == expected
+    assert distributor.distributed(accounts[1], three_gauges[0]) == expected
 
 
-def test_mint_multiple_gauges(accounts, chain, three_gauges, minter, token):
+def test_mint_multiple_gauges(accounts, chain, three_gauges, distributor, token):
     for i in range(3):
         three_gauges[i].deposit((i + 1) * 10 ** 17, {"from": accounts[1]})
 
     chain.sleep(MONTH)
 
     for i in range(3):
-        minter.mint(three_gauges[i], {"from": accounts[1]})
+        distributor.distribute(three_gauges[i], {"from": accounts[1]})
 
-    total_minted = 0
+    total_dist = 0
     for i in range(3):
         gauge = three_gauges[i]
-        minted = minter.minted(accounts[1], gauge)
-        assert minted == gauge.integrate_fraction(accounts[1])
-        total_minted += minted
+        distributed = distributor.distributed(accounts[1], gauge)
+        assert distributed == gauge.integrate_fraction(accounts[1])
+        total_dist += distributed
 
-    assert token.balanceOf(accounts[1]) == total_minted
+    assert token.balanceOf(accounts[1]) == total_dist
 
 
-def test_mint_after_withdraw(accounts, chain, three_gauges, minter, token):
+def test_mint_after_withdraw(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
     chain.sleep(2 * WEEK)
     three_gauges[0].withdraw(1e18, {"from": accounts[1]})
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
 
     assert token.balanceOf(accounts[1]) > 0
 
 
-def test_mint_multiple_after_withdraw(accounts, chain, three_gauges, minter, token):
+def test_mint_multiple_after_withdraw(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
     chain.sleep(10)
     three_gauges[0].withdraw(1e18, {"from": accounts[1]})
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
     balance = token.balanceOf(accounts[1])
 
     chain.sleep(10)
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
 
+    # shouldn't have distributed any new tokens to accounts[1]
     assert token.balanceOf(accounts[1]) == balance
 
 
-def test_no_deposit(accounts, chain, three_gauges, minter, token):
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+def test_no_deposit(accounts, chain, three_gauges, distributor, token):
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
 
     assert token.balanceOf(accounts[1]) == 0
-    assert minter.minted(accounts[1], three_gauges[0]) == 0
+    assert distributor.distributed(accounts[1], three_gauges[0]) == 0
 
 
-def test_mint_wrong_gauge(accounts, chain, three_gauges, minter, token):
+def test_mint_wrong_gauge(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
     chain.sleep(MONTH)
-    minter.mint(three_gauges[1], {"from": accounts[1]})
+    distributor.distribute(three_gauges[1], {"from": accounts[1]})
 
     assert token.balanceOf(accounts[1]) == 0
-    assert minter.minted(accounts[1], three_gauges[0]) == 0
-    assert minter.minted(accounts[1], three_gauges[1]) == 0
+    assert distributor.distributed(accounts[1], three_gauges[0]) == 0
+    assert distributor.distributed(accounts[1], three_gauges[1]) == 0
 
 
-def test_mint_not_a_gauge(accounts, minter):
+def test_mint_not_a_gauge(accounts, distributor):
     with brownie.reverts("dev: gauge is not added"):
-        minter.mint(accounts[1], {"from": accounts[0]})
+        distributor.distribute(accounts[1], {"from": accounts[0]})
 
 
-def test_mint_before_inflation_begins(accounts, chain, three_gauges, minter, token):
+def test_mint_before_inflation_begins(accounts, chain, three_gauges, distributor, token):
     three_gauges[0].deposit(1e18, {"from": accounts[1]})
 
-    chain.sleep(token.start_epoch_time() - chain.time() - 5)
-    minter.mint(three_gauges[0], {"from": accounts[1]})
+    chain.sleep(distributor.start_epoch_time() - chain.time() - 5)
+    distributor.distribute(three_gauges[0], {"from": accounts[1]})
 
     assert token.balanceOf(accounts[1]) == 0
-    assert minter.minted(accounts[1], three_gauges[0]) == 0
+    assert distributor.distributed(accounts[1], three_gauges[0]) == 0
